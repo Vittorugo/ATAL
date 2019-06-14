997beccb8fdac8ddebdf98a2efc5621f546919ef
Victtor Hugo <hgloureirocosta@gmail.com>
2019-06-14 20:34:53 -0300

    Correções feitas. Algoritmo encontrando a saída.

--- labirinto.py
+++ labirinto.py
@@ -1,36 +1,36 @@
 def print_solution(caminho):
-    
+
     for i in range(len(caminho)):
         print(caminho[i], end="\n")
 
 
-def caminha_labirinto(lab,x,y,sx,sy,solucao):
+def caminha_labirinto(lab, pos_linha, pos_coluna, pos_linha_saida, pos_coluna_saida, solucao):
 
-    if x == sx and y == sy:
-        solucao[x][y] = "S"
+    if pos_linha == pos_linha_saida and pos_coluna == pos_coluna_saida:
+        solucao[pos_linha][pos_coluna] = "S"
         print_solution(solucao)
         return True
 
-    elif ( (0 <= y < len(lab)) and (0 <= x < len(lab))):
+    elif ( (0 <= pos_linha < len(lab)) and (0 <= pos_coluna < len(lab))):
 
-        if ((lab[x][y] == " ") and (solucao[x][y] == " ")):
-            solucao[x][y] = "*"
+        if ((lab[pos_linha][pos_coluna] == " ") and (solucao[pos_linha][pos_coluna] == " ")):
+            solucao[pos_linha][pos_coluna] = "*"
 
-            if (caminha_labirinto(lab, x, y+1, sx, sy, solucao)): # percorrendo labirinto para direita
+            if (caminha_labirinto(lab, pos_linha, pos_coluna+1, pos_linha_saida, pos_coluna_saida, solucao)): # percorrendo labirinto para direita
                 return True
 
-            elif(caminha_labirinto(lab, x+1, y, sx, sy, solucao)): # percorrendo labirinto para cima
+            elif(caminha_labirinto(lab, pos_linha+1, pos_coluna, pos_linha_saida, pos_coluna_saida, solucao)): # percorrendo labirinto para cima
                 return True
 
-            elif(caminha_labirinto(lab, x, y-1, sx, sy, solucao)): # percorrendo labirinto para esquerda
+            elif(caminha_labirinto(lab, pos_linha, pos_coluna-1, pos_linha_saida, pos_coluna_saida, solucao)): # percorrendo labirinto para esquerda
                 return True
 
-            elif(caminha_labirinto(lab, x-1, y, sx, sy, solucao)): # percorrendo labirinto para baixo
+            elif(caminha_labirinto(lab, pos_linha-1, pos_coluna, pos_linha_saida, pos_coluna_saida, solucao)): # percorrendo labirinto para baixo
                 return True
 
             else:
-                solucao[x][y] = " "
-                
+                solucao[pos_linha][pos_coluna] = " "
+
         else:
             return False
 
@@ -45,4 +45,8 @@ lab = [[" "," "," "," ",'#'],
 
 caminho_saida = lab.copy()
 
-caminha_labirinto(lab, 0, 0, 4, 4, caminho_saida)
+entrada = [0,0]
+saida   = [4,4]
+
+
+caminha_labirinto(lab, entrada[0],entrada[1], saida[0], saida[1], caminho_saida)
