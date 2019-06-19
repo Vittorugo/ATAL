be5cfbdea08aa32e573393ea787e2b837413480a
Victtor Hugo <hgloureirocosta@gmail.com>
2019-06-19 13:25:33 -0300

    Alterações realizadas para que a questão funcione corretamente e seja aprovada no URI

--- desenho_labirinto.py
+++ desenho_labirinto.py
@@ -9,32 +9,33 @@ class Grafo():
         self.vertices    = [] #lista contendo os vertices do grafo
         self.arestas     = [] #lista contendo as arestas  do grafo
         self.descoberto  = [] #lista de vertices descobertos
-        self.pai         = [] #vertices pai
-        self.salto       = [] #lista com saltos
-
+        self.adjacencias = [] #lista de  adjacentes
 # Busca em profundidade
 
 def DFS_visit(grafo, vertice):
 
-    global tempo
+    global contagem
+    global vertice_inicio
 
-    tempo += 1
+    if (vertice_inicio != vertice):
+        contagem += 1
 
-    grafo.descoberto[vertice] = tempo
-    for i in grafo.arestas[vertice]:
+    grafo.descoberto[vertice] = 1
+    for i in grafo.adjacencias[vertice]:
         if (grafo.descoberto[i] == 0):
             DFS_visit(grafo,i)
             grafo.pai[i] = vertice
 
 
-    tempo += 1
-    grafo.salto[vertice] = tempo
+    #tempo += 1
+    #grafo.salto[vertice] =
 
 def DFS(grafo):
 
     for vertice in grafo.vertices:
         if( grafo.descoberto[vertice] == 0):
-            DFS_visit(grafo, vertice)
+            if (vertice_inicio != vertice):
+                DFS_visit(grafo, vertice)
 
 
 def prepara_proxima_entrada(grafo):
@@ -42,9 +43,7 @@ def prepara_proxima_entrada(grafo):
     grafo.vertices   = []  # limpa lista contendo os vertices do grafo
     grafo.arestas    = []  # limpa lista contendo as arestas  do grafo
     grafo.descoberto = []  # limpa lista de vertices descobertos
-    grafo.pai        = []  # limpa lista vertices pai
-    grafo.salto      = []  # limpa lista com saltos
-
+    grafo.adjacencias= []  # limpa lista de adjacencias
 
 grafo = Grafo()
 check = 0
@@ -64,7 +63,7 @@ while True:
 
 while (check != casos_teste):
 
-        tempo = 0
+        contagem = 0
 
         # 2º linha de entrada: Vertice inicial onde o desenho deve ser iniciado.
 
@@ -91,9 +90,9 @@ while (check != casos_teste):
         for i in range(quant_vertices): # adicionando os vertices da instância Grafo criada.
             grafo.vertices.append(i)
             grafo.descoberto.append(0)  # alocando indices para inserir os vertices descobertos.
-            grafo.pai.append(0)         # alocando indices para depois inserir os nós que são pais.
+            grafo.adjacencias.append([])# alocando nossa lista de adjacencia para colocar os vertices vizinhos conforme indice.
 
-        for i in range(quant_arestas): # criando a matriz de adjacencias para inserir as arestas.
+        for i in range(quant_arestas): # criando a matriz para inserir as arestas.
             grafo.arestas.append([])
 
         cont = 0
@@ -113,8 +112,36 @@ while (check != casos_teste):
 
                 cont += 1
 
+
+        # Alocar a lista de adjacencia ....
+
+        for i in range(quant_arestas):
+
+            if ( grafo.arestas[i][0] in grafo.adjacencias[grafo.arestas[i][1]]):
+                pass
+            else:
+                grafo.adjacencias[grafo.arestas[i][1]].append(grafo.arestas[i][0])
+
+            if ( grafo.arestas[i][1] in grafo.adjacencias[grafo.arestas[i][0]]):
+                pass
+            else:
+                grafo.adjacencias[grafo.arestas[i][0]].append(grafo.arestas[i][1])
+
+
+        # podem existir nós desconexos no grafo tempo que marcalos pois eles não seram alcançados pela busca em profundidade.
+
+        for i in range(quant_vertices):
+
+            if ( len(grafo.adjacencias[i]) == 0):
+                grafo.descoberto[i] = "None"
+
+        #chamando a busca em pronfundidade
+
         DFS(grafo)
-        list_temp.append(tempo)
+        list_temp.append(contagem*2) # multiplico por dois pois em um grafo conexo se você quer partir de um ponto visitar todos os vertices e voltar para esse mesmo ponto o tamanho do caminho percorrido é igual a quantidade de vertices visitados vezes 2.
+
+        #print(grafo.descoberto) para vizualizar os nós que foram descobertos. Nós desconexos não são visitados.
+
         prepara_proxima_entrada(grafo)
 
         check += 1
